8fd92eabb612aaedda44f4057ff3fe0eaa8686e2
Stefan Ihle <stefanihle@gmx.net>
2022-04-10 19:43:08 +0200

    add signup functionallity as well as basic validation

---
+++ src/resolvers/Mutation/auth.ts
@@ -0,0 +1,76 @@
+import validator from 'validator';
+import { Context } from '../../index';
+
+interface SignupArgs {
+	email: string;
+	name: string;
+	bio: string;
+	password: string;
+}
+
+interface UserPayload {
+	userErrors: {
+		message: string;
+	}[];
+	user: null;
+}
+
+export const authResolvers = {
+	signup: async (
+		_: any,
+		{ email, name, password, bio }: SignupArgs,
+		{ prisma }: Context
+	): Promise<UserPayload> => {
+		const isEmail = validator.isEmail(email);
+
+		if (!isEmail) {
+			return {
+				userErrors: [
+					{
+						message: 'Invalid email'
+					}
+				],
+				user: null
+			};
+		}
+
+		const isValidPassword = validator.isLength(password, {
+			min: 5
+		});
+
+		if (!isValidPassword) {
+			return {
+				userErrors: [
+					{
+						message: 'Invalid password'
+					}
+				],
+				user: null
+			};
+		}
+
+		if (!name || !bio) {
+			return {
+				userErrors: [
+					{
+						message: 'Invalid name or bio'
+					}
+				],
+				user: null
+			};
+		}
+
+		return {
+			userErrors: [],
+			user: null
+		};
+
+		// return prisma.user.create({
+		//   data: {
+		//     email,
+		//     name,
+		//     password
+		//   }
+		// })
+	}
+};
